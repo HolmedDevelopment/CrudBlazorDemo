08f21f5f1396a22e8de6dbd2652d2c0f7c2b30b3
HolmedMhmdT <mhamad.tabikh@holmedgroup.com>
2022-08-25 16:56:50 +0300

    Add project files.

---
+++ AspCoreBlazorTutorial/Server/Controllers/PeopleController.cs
@@ -0,0 +1,60 @@
+﻿using AspCoreBlazorTutorial.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreBlazorTutorial.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public static List<Person> People = new List<Person>()
+        {
+            new Person()
+            {
+                Id = 1,FirstName = "Mhmd",LastName = "Tabikh",Sex = Sex.Male
+            },
+            new Person()
+            {
+                Id = 2,FirstName = "Daniel",LastName = "Jreige",Sex = Sex.Male
+            },
+            new Person()
+            {
+                Id = 3,FirstName = "SaraH",LastName = "Salim",Sex = Sex.Female
+            },
+                   new Person()
+            {
+                Id = 4,FirstName = "Charles",LastName = "Fenianos",Sex = Sex.Male
+            },
+                          new Person()
+            {
+                Id = 5,FirstName = "Carmen",LastName = "Kaouk",Sex = Sex.Female
+            },
+        };
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetPeople()
+        {
+            return Ok(People);
+        }
+
+        //will throw ambigous exception if uncommented when calling /api/people
+        //[HttpGet] 
+        //public async Task<IActionResult> GetPeoplez()
+        //{
+        //    return NotFound();
+        //}
+
+
+        // will be called on /api/people/1
+        [HttpGet("{personId}")]
+        public async Task<IActionResult> GetPersonzasw(int personId) 
+        {
+            var person = People.FirstOrDefault(x => x.Id == personId);
+            if (person == null)
+                return NotFound("Sorry :(");
+            return Ok(person);
+        }
+    }
+}
---
+++ AspCoreBlazorTutorial/Shared/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspCoreBlazorTutorial.Shared
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public Sex Sex { get; set; }
+    }
+    public enum Sex
+    {
+        Male =1,Female,Other
+    }
+}
